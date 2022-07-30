e294da1edd09c2a8cc37126dd21dcb8a5a65d326
roninro <liumy.f@foxmail.com>
2022-07-30 15:42:31 +0800

    toc endLevel h4

--- assets/js/hux-blog.js
+++ assets/js/hux-blog.js
@@ -1,7 +1,7 @@
- /*!
+/*!
  * Hux Blog v1.6.0 (http://startbootstrap.com)
  * Copyright 2016 @huxpro
- * Licensed under Apache 2.0 
+ * Licensed under Apache 2.0
  */
 
 // Tooltip Init
@@ -10,9 +10,8 @@
 //     $("[data-toggle='tooltip']").tooltip();
 // });
 
-
 // make all images responsive
-/* 
+/*
  * Unuse by Hux
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
@@ -22,57 +21,68 @@
 // });
 
 // responsive tables
-$(document).ready(function() {
-    $("table").wrap("<div class='table-responsive'></div>");
-    $("table").addClass("table");
+$(document).ready(function () {
+  $("table").wrap("<div class='table-responsive'></div>");
+  $("table").addClass("table");
 });
 
 // responsive embed videos
-$(document).ready(function() {
-    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
-    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+$(document).ready(function () {
+  $('iframe[src*="youtube.com"]').wrap(
+    '<div class="embed-responsive embed-responsive-16by9"></div>'
+  );
+  $('iframe[src*="youtube.com"]').addClass("embed-responsive-item");
+  $('iframe[src*="vimeo.com"]').wrap(
+    '<div class="embed-responsive embed-responsive-16by9"></div>'
+  );
+  $('iframe[src*="vimeo.com"]').addClass("embed-responsive-item");
 });
 
 // Navigation Scripts to Show Header on Scroll-Up
-jQuery(document).ready(function($) {
-    var MQL = 1170;
+jQuery(document).ready(function ($) {
+  var MQL = 1170;
 
-    //primary navigation slide-in effect
-    if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height(),
-            bannerHeight  = $('.intro-header .container').height();     
-        $(window).on('scroll', {
-                previousTop: 0
-            },
-            function() {
-                var currentTop = $(window).scrollTop(),
-                    $catalog = $('.side-catalog');
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $(".navbar-custom").height(),
+      bannerHeight = $(".intro-header .container").height();
+    $(window).on(
+      "scroll",
+      {
+        previousTop: 0,
+      },
+      function () {
+        var currentTop = $(window).scrollTop(),
+          $catalog = $(".side-catalog");
 
-                //check if user is scrolling up by mouse or keyborad
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
+        //check if user is scrolling up by mouse or keyborad
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $(".navbar-custom").hasClass("is-fixed")) {
+            $(".navbar-custom").addClass("is-visible");
+          } else {
+            $(".navbar-custom").removeClass("is-visible is-fixed");
+          }
+        } else {
+          //if scrolling down...
+          $(".navbar-custom").removeClass("is-visible");
+          if (
+            currentTop > headerHeight &&
+            !$(".navbar-custom").hasClass("is-fixed")
+          )
+            $(".navbar-custom").addClass("is-fixed");
+        }
+        this.previousTop = currentTop;
 
-
-                //adjust the appearance of side-catalog
-                $catalog.show()
-                if (currentTop > (bannerHeight + 41)) {
-                    $catalog.addClass('fixed')
-                } else {
-                    $catalog.removeClass('fixed')
-                }
-            });
-    }
+        //adjust the appearance of side-catalog
+        $catalog.show();
+        if (currentTop > bannerHeight + 41) {
+          $catalog.addClass("fixed");
+        } else {
+          $catalog.removeClass("fixed");
+        }
+      }
+    );
+  }
 });
+
